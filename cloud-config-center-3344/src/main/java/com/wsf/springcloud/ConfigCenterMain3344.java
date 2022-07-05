42f9a013a621cad1f169eda3aaaed892bc4b1782
Wangsuifan <wsf_work@163.com>
2022-07-05 23:19:22 +0800

    Config完成

---
+++ cloud-config-center-3344/src/main/java/com/wsf/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.wsf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wsfstart
+ * @create 2022-07-05 22:31
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigCenterMain3344 {
+  public static void main(String[] args) {
+      SpringApplication.run(ConfigCenterMain3344.class,args);
+  }
+}
