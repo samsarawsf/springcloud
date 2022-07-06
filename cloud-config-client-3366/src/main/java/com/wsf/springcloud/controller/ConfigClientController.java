0cd1c08b7f8cc357ef8bdfa4138fe640ab9b1234
Wangsuifan <wsf_work@163.com>
2022-07-06 12:29:37 +0800

    Bus完成

---
+++ cloud-config-client-3366/src/main/java/com/wsf/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.wsf.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wsfstart
+ * @create 2022-07-05 22:56
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
